212b9a3b1f88422357bbef054e3897a5c8626b0d
mathdothings <jhonata.prodrigues@gmail.com>
2021-04-01 20:46:17 -0300

    done

--- eloquent-js/exercises/recursion/index.js
+++ eloquent-js/exercises/recursion/index.js
@@ -1,3 +1,17 @@
+/* 
+We’ve seen that % (the remainder operator) can be used to test whether a
+number is even or odd by using % 2 to see whether it’s divisible by two. Here’s
+another way to define whether a positive whole number is even or odd:
+• Zero is even.
+• One is odd.
+• For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and return
+a Boolean.
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
+way to fix this? 
+*/
+
 function isEven(n) {
   if (n < 0) return isEven(Math.abs(n));
   if (n == 0 ) return true;
@@ -7,4 +21,7 @@ function isEven(n) {
   }
 }
 
-console.log(isEven(-15));
+console.log(isEven(0)); // true
+console.log(isEven(1)); // false
+console.log(isEven(10)); // true
+console.log(isEven(-1)); // false
