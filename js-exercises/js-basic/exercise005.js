ca31c29f43c674697d736069da37ac4127465b95
mathdothings <jhonata.prodrigues@gmail.com>
2021-08-29 06:25:31 -0300

    todo: setInterval

--- js-exercises/js-basic/exercise005.js
+++ js-exercises/js-basic/exercise005.js
@@ -1,16 +1,11 @@
 function rotateWord() {
-  let string = document.getElementById('target');
-  let reversedWord = '';
+  let string = document.getElementById('target').textContent;
+  let rotatedWord = '';
 
   for (let letter = string.length - 1; letter >= 0; letter--) {
-    reversedWord += string.substring(letter, letter + 1);
+    rotatedWord += string.substring(letter, letter + 1);
   }
 
-  document.getElementById('target').innerHTML = reversedWord;
+  document.getElementById('target').textContent = rotatedWord;
 }
 // ECROUSER3W
-
-function changeName() {
-  let word = document.getElementById('target');
-  word.innerHTML = "mathdothings";
-}
