c0f0e965ed6256b87bb9284a9a201b8362aa4dc9
mathdothings <jhonata.prodrigues@gmail.com>
2021-09-01 08:33:15 -0300

    done

---
+++ js-exercises/js-oop-basic/exercise01.js
@@ -0,0 +1,9 @@
+let student = {
+  name: "mathdothings",
+  sclass: 'VI',
+  rollno: 12
+}
+
+console.log(student.name, student.sclass, student.rollno);
+console.log();
+console.log(`Name ${student.name}\nClass: ${student.sclass}\nRoll: ${student.rollno}`);
