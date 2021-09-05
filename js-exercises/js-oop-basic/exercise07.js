8bc19dc56b01982d71847cf8e0c456d0189bce83
mathdothings <jhonata.prodrigues@gmail.com>
2021-09-05 00:55:44 -0300

    todo

---
+++ js-exercises/js-oop-basic/exercise07.js
@@ -0,0 +1,16 @@
+String.prototype.subset = function() {
+  let subsetStr = [];
+
+  function calcPossibilities(this.length) {
+
+    if (this.length <= 1) return 1;
+    else {
+      return this.length * calcPossibilities(num - 1);
+    }
+
+    for (let x = 0; x < calcPossibilities(this.length); x++) {
+      
+    }
+
+  return subsetStr;
+}
