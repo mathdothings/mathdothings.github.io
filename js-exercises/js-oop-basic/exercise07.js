e2cfd8496bccd26c88d3cb355be9d185527f2062
mathdothings <jhonata.prodrigues@gmail.com>
2021-09-07 09:57:55 -0300

    done

--- js-exercises/js-oop-basic/exercise07.js
+++ js-exercises/js-oop-basic/exercise07.js
@@ -1,16 +1,34 @@
 String.prototype.subset = function() {
-  let subsetStr = [];
+  let result = [];
+  let index = 0;
+  let gap = 0;
 
-  function calcPossibilities(this.length) {
-
-    if (this.length <= 1) return 1;
+  // calculate amount of possibilities
+  function factorial(n) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
     else {
-      return this.length * calcPossibilities(num - 1);
+      return n * factorial(n - 1);
     }
+  }
 
-    for (let x = 0; x < calcPossibilities(this.length); x++) {
-      
+  for (let x = 0; x < factorial(this.length); x++) {
+    if (x >= this.length) {
+      if (x === this.length) {
+        index = Math.round(((x / 2) - 1));
+        gap = 1;
+      }  
+      index = Math.round(((x / 2) - 1));
+      gap++;
     }
+    else {
+      index = 0;
+      gap++;
+    }
+    result.push(this.substring(index, gap));
+  }
 
-  return subsetStr;
+  return result;
 }
+
+console.log("dog".subset()); // ==> ["d", "do", "dog", "o", "og, "g"].
