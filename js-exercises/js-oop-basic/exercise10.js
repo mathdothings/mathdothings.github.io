d00e8ed955ffc3634e6e1e3b50df05f6dfc19ad4
mathdothings <jhonata.prodrigues@gmail.com>
2021-09-11 09:20:53 -0300

    todo

---
+++ js-exercises/js-oop-basic/exercise10.js
@@ -0,0 +1,21 @@
+// Write a JavaScript program to sort an array of JavaScript objects.
+
+let library = [
+  {
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
+    libraryID: 1254
+  },
+  {
+    title: 'Walter Isaacson',
+    author: 'Steve Jobs',
+    libraryID: 4264
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzane Collins',
+    libraryID: 3245
+  }
+]
+
+console.log(library.sort());
